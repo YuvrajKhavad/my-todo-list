5619e948fe249458b0dfd58da359822898ff51b3
yuvrajkhavad <yuvraj.khavad@gmail.com>
2020-12-24 23:08:43 +0530

    script set up

---
+++ includes/my-todo-list-script.php
@@ -0,0 +1,20 @@
+<?php 
+    // Check if admiin
+    if(is_admin())
+    {
+        // Add admin scripts
+        function mtl_add_admin_script()
+        {
+            wp_enqueue_style( 'mtl-admin-style', MTL_PLUGIN_CSS . 'style-admin.css');
+        }
+        add_action('admin_init', 'mtl_add_admin_script');
+    }
+
+    // add front js and css
+    function mtl_add_script()
+    {
+        wp_enqueue_style( 'mtl-style', MTL_PLUGIN_CSS . 'style.css');
+        wp_enqueue_script( 'mtl-js', MTL_PLUGIN_JS . 'main.js' , array(jquery));
+    }
+    add_action('wp_enqueue_scripts', 'mtl_add_script')
+?>
---
+++ my-todo-list.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * Plugin Name: My Todo List
+ * Description: A simple todo list plugin
+ * Version: 1.0
+ * Author: Yuvraj Khavad
+ *
+ **/
+// Exit if accessed directly
+if(!defined('ABSPATH'))
+{
+    exit;
+}
+
+/**
+ * Define Constants
+ */
+define('MTL_PLUGIN_PATH', plugin_dir_path(__FILE__).'/');
+define('MTL_INCLUDES', MTL_PLUGIN_PATH .'includes/');
+
+define('MTL_PLUGIN_URL', plugins_url().'/my-todo-list/');
+define('MTL_URL_INCLUDES', MTL_PLUGIN_URL.'includes/');
+define('MTL_PLUGIN_JS', MTL_PLUGIN_URL.'js/');
+define('MTL_PLUGIN_CSS', MTL_PLUGIN_URL.'css/');
+
+// Load scripts
+require_once(MTL_INCLUDES. 'my-todo-list-script.php');
+
+// Load shortcodes
+require_once(MTL_INCLUDES. 'my-todo-list-shortcodes.php');
+
+//check if admin
+if(is_admin())
+{
+    // Load custome post type
+    require_once(MTL_INCLUDES. 'my-todo-list-cpt.php');
+
+    // Load custome filed
+    require_once(MTL_INCLUDES. 'my-todo-list-field.php');
+}
+
+// Register Widget
+/*function register_newsletter_subscriber()
+{
+    //register_widget('newsletter_subscriber_widget');
+}
+//add_action('widgets_init', 'register_newsletter_subscriber');*/
+?>
